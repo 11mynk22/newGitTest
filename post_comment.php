6e2c24aed180827200495bc72ee7f8be47b89864
Mayank <mayank.chandelkar8@gmail.com>
2017-12-05 20:10:48 +0530

    Add files via upload

--- post_comment.php
+++ post_comment.php
@@ -1,13 +1,24 @@
-<?php
+<?php  
 require('Persistence.php');
 
 $db = new Persistence();
 $added = $db->add_comment($_POST);
 
-if($added) {
-  header( 'Location: index.php' );
-}
+
+include("database/db_conection.php");//make connection here  
+if(isset($_POST['submit']))  
+{  
+    $your_name=$_POST['comment_author'];//here getting result from the post array after submitting the form.  
+    $your_email=$_POST['email'];//same  
+    $your_comment=$_POST['comment'];//same  
+
+    $insert_user="insert into data (user_name,user_email,user_comment) VALUE ('$your_name','$your_email','$your_comment')";  
+    if(mysqli_query($dbcon,$insert_user))  
+    {  
+        echo"<script>window.open('welcome.php','_self')</script>";  
+    }  
 else {
-  header( 'Location: index.php?error=Your comment was not posted due to errors in your form submission' );
+  header( 'Location: welcome.php?error=Your comment was not posted due to errors in your form submission' );
+}
 }
 ?>

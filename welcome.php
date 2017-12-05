38b62bc1c5769f527df09b3d7bf4a609cf880cfb
Mayank <mayank.chandelkar8@gmail.com>
2017-12-05 10:07:48 +0530

    Add files via upload

--- welcome.php
+++ welcome.php
@@ -1,37 +1,75 @@
-<?php  
-session_start();  
-  
-if(!$_SESSION['email'])  
-{  
-    header("Location: login.php");//redirect to login page to secure the welcome page without login access.  
-}  
+<?php
+
+require('Persistence.php');
+$comment_post_ID = 1;
+$db = new Persistence();
+$comments = $db->get_comments($comment_post_ID);
+$has_comments = (count($comments) > 0);
 ?>
 
-<html>
+    <!DOCTYPE html>
+    <html lang="en-US">
 
-<head>
+    <head>
+        <title>Smashing HTML5!</title>
+        <meta charset="utf-8" />
 
-    <title>
-        Registration
-    </title>
-    <script src="css/bootstrap.css"></script>
-</head>
+        <link rel="stylesheet" href="css/main.css" type="text/css" />
 
-<body>
-    <h1>Welcome</h1>
-    <?php  
+        <!--[if IE]>
+	  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+        <!--[if lte IE 7]>
+		<link rel="stylesheet" type="text/css" media="all" href="css/ie.css"/>
+		<script src="js/IE8.js" type="text/javascript"></script><![endif]-->
+        <!--[if lt IE 7]>
+		<link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/><![endif]-->
+
+    </head>
+
+    <body id="index" class="home">
+
+        <header id="banner" class="body">
+            <h1><a href="#">Example <br><strong>The most intelligent way to improve vocabulary</strong></a></h1>
+            <nav>
+                <ul>
+                    <li class="active"><a href="#">
+                    Home</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li style="color: white;">
+                        <?php  
     echo $_SESSION['email'];  
     ?>
-    <p><a href="logout.php">Logout here</a> </p>
-    <br><br>
+                    </li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </nav>
+        </header>
 
-    <form role="form" action="welcome.php" method="post">
-        <input type="text" name="name" placeholder="Enter a word" required>
+        <section id="content" class="body">
 
-        <input name="display" class="btn btn-danger" type="submit" value="Search">
-    </form>
+            <article class="hentry">
+                <header>
+                    <h2 class="entry-title"><a href="#" rel="bookmark" title="Permalink to this Building a Pusher-powered Real-Time Commenting System">Example.com combines the world's smartest dictionary with an adaptive learning</a></h2>
+                </header>
 
-    <?php  
+                <footer class="post-info">
+                    <abbr class="published" title="2017-12-2T14:07:00-07:00">
+					3rd Dec 2017
+				</abbr>
+
+                    <address class="vcard author">
+					By <a class="url fn" href="#">Mynk</a>
+				</address>
+                </footer>
+
+                <div class="entry-content">
+                    <form role="form" action="welcome.php" method="post">
+                        <input type="text" name="name" placeholder="Enter a word" required>
+
+                        <input name="display" class="btn btn-danger" type="submit" value="Search">
+                    </form>
+                    <?php  
   
 include("database/db_conection.php");//make connection here  
 if(isset($_POST['display']))  
@@ -46,9 +84,10 @@ if(isset($_POST['display']))
     if(mysqli_num_rows($run_query)>0)
     {
         while($row=mysqli_fetch_array($run_query)){
-            echo $row[0];
+            echo "</br></br>";
+            echo "<strong>$row[0]</strong>";
             echo ":</br></br>";
-            echo $row[1];
+            echo "<strong>Definition:</strong> $row[1]";
             echo "</br></br>";
         }
     }
@@ -56,14 +95,129 @@ if(isset($_POST['display']))
   
 ?>
 
-    <form action="welcome.php" role="form" method="post">
-        <textarea type="text" name="user_comment" placeholder="Add a comment"></textarea>
-        <input type="submit" name="post" value="Post">
-    </form>
+                </div>
+            </article>
+
+        </section>
 
 
+        <section id="comments" class="body">
+
+            <header>
+                <h2>Comments</h2>
+            </header>
+
+            <ol id="posts-list" class="hfeed<?php echo($has_comments?' has-comments':''); ?>">
+                <li class="no-comments">Be the first to add a comment.</li>
+                <?php
+        foreach ($comments as &$comment) {
+          ?>
+                    <li>
+                        <article id="comment_<?php echo($comment['id']); ?>" class="hentry">
+                            <footer class="post-info">
+                                <abbr class="published" title="<?php echo($comment['date']); ?>">
+    						<?php echo( date('d F Y', strtotime($comment['date']) ) ); ?>
+    					</abbr>
+
+                                <address class="vcard author">
+    						By <a class="url fn" href="#"><?php echo($comment['comment_author']); ?></a>
+    					</address>
+                            </footer>
+
+                            <div class="entry-content">
+                                <p>
+                                    <?php echo($comment['comment']); ?>
+                                </p>
+                            </div>
+                        </article>
+                    </li>
+                    <?php
+        }
+      ?>
+            </ol>
+
+            <div id="respond">
+
+                <h3>Leave a Comment</h3>
+
+                <form action="post_comment.php" method="post" id="commentform">
+
+                    <label for="comment_author" class="required">Your name</label>
+                    <input type="text" name="comment_author" id="comment_author" value="" tabindex="1" required="required">
+
+                    <label for="email" class="required">Your email</label>
+                    <input type="email" name="email" id="email" value="" tabindex="2" required="required">
+
+                    <label for="comment" class="required">Your message</label>
+                    <textarea name="comment" id="comment" rows="10" tabindex="4" required="required"></textarea>
+
+                    <input type="hidden" name="comment_post_ID" value="<?php echo($comment_post_ID); ?>" id="comment_post_ID" />
+                    <input name="submit" type="submit" value="Submit comment" />
+
+                </form>
+
+            </div>
+
+        </section>
+
+        <section id="extras" class="body">
+            <div class="blogroll">
+                <h2>blogroll</h2>
+                <ul>
+                    <li><a href="#" rel="external">HTML5 Doctor</a></li>
+                    <li><a href="#" rel="external">HTML5 Spec (working draft)</a></li>
+                    <li><a href="#" rel="external">Smashing Magazine</a></li>
+                    <li><a href="#" rel="external">W3C</a></li>
+                    <li><a href="#" rel="external">Wordpress</a></li>
+                    <li><a href="#" rel="external">Wikipedia</a></li>
+
+                    <li><a href="#" rel="external">HTML5 Doctor</a></li>
+                    <li><a href="#" rel="external">HTML5 Spec (working draft)</a></li>
+                    <li><a href="#" rel="external">Smashing Magazine</a></li>
+                    <li><a href="#" rel="external">W3C</a></li>
+                    <li><a href="#" rel="external">Wordpress</a></li>
+                    <li><a href="#" rel="external">Wikipedia</a></li>
+
+                    <li><a href="#" rel="external">HTML5 Doctor</a></li>
+                    <li><a href="#" rel="external">HTML5 Spec (working draft)</a></li>
+                    <li><a href="#" rel="external">Smashing Magazine</a></li>
+                    <li><a href="#" rel="external">W3C</a></li>
+                    <li><a href="#" rel="external">Wordpress</a></li>
+                    <li><a href="#" rel="external">Wikipedia</a></li>
+                    <article class="hentry">
+                </ul>
+            </div>
+
+            <div class="social">
+                <h2>social</h2>
+                <ul>
+                    <li><a href="http://delicious.com/">delicious</a></li>
+                    <li><a href="http://digg.com/users/">digg</a></li>
+                    <li><a href="http://facebook.com/">facebook</a></li>
+                    <li><a href="http://www.last.fm">last.fm</a></li>
+                    <li><a href="http://website.com/feed/" rel="alternate">rss</a></li>
+                    <li><a href="http://twitter.com/">twitter</a></li>
+                </ul>
+            </div>
+        </section>
+
+        <footer id="contentinfo" class="body">
+            <address id="about" class="vcard body">
+			<span class="primary">
+				<strong><a href="#" class="fn url">Smashing Magazine</a></strong>
+				<span class="role">Amazing Magazine</span>
+			</span>
+		
+			<img src="images/avatar.gif" alt="Smashing Magazine Logo" class="photo" />
+		
+			<span class="bio">Smashing Magazine is a website and blog that offers resources and advice to web developers and web designers. It was founded by Sven Lennartz and Vitaly Friedman.</span>
+		
+		</address>
+
+            <p>20017 &copy; <a href="http://smashingmagazine.com">Group</a>.</p>
+        </footer>
 
 
-</body>
+    </body>
 
-</html>
+    </html>
